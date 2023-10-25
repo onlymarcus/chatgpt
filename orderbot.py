cdc77571a3d347d547de06bbfcf665b175813dd9
Marcus <marcusjunior@gmail.com>
2023-10-25 14:51:47 -0300

    importei streamlit

--- orderbot.py
+++ orderbot.py
@@ -3,4 +3,6 @@ import openai
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv()) # read local .env file
 
-
+import streamlit as st
+st.title('My first app')
+st.write('Hello world')
