b54bf7bae66a74bbd985053cb268eb88178dc948
Marcus <marcusjunior@gmail.com>
2023-10-27 16:52:35 -0300

    adicionei um código para solicitar a apikey do usuário antes e rodar o programa

--- chatgpt-like.py
+++ chatgpt-like.py
@@ -1,9 +1,15 @@
 import openai
 import streamlit as st
 
+with st.sidebar:
+    openai_api_key = st.text_input(
+        "OpenAI API Key", key="chatbot_api_key", type="password")
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+
 st.title("ChatGPT-like clone")
 
-openai.api_key = st.secrets["api_secret"]
+# openai.api_key = st.secrets["api_secret"]
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
@@ -16,6 +22,10 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 if prompt := st.chat_input("What is up?"):
+    if not openai_api_key:
+        st.info("Please add your Openai API Key to continue")
+        st.stop()
+    openai.api_key = openai_api_key
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
